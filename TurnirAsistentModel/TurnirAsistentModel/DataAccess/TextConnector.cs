a2a11d4ddd4b13dc396d029a0ed7ad49b1f0b14b
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-05-05 12:03:36 +0200

    Add files via upload

---
+++ TurnirAsistentModel/TurnirAsistentModel/DataAccess/TextConnector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnirAsistentModel.Models;
+
+namespace TurnirAsistentModel.DataAccess
+{
+    public class TextConnector : IDataConnection
+    {
+        public NagradaModel NapraviNagradu(NagradaModel model)
+        {
+            model.ID = 1;
+            return model;
+        }
+    }
+}
